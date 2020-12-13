f83363c637f984fde51b2397e52aa0c00dd004ae
Chris Han <christopherhan@utexas.edu>
2020-12-13 14:02:10 -0600

    added picture of me

--- layouts.py
+++ layouts.py
@@ -1,10 +1,11 @@
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
+from app import app
 
 layout_bio = html.Div([
+    html.Img(src=app.get_asset_url('profile.jpg')),
     html.P("It's a me, Chris")
-    
 ])
 
 layout_portfolio = html.Div([
